b9c710360d3146b5da663a2034e332ce60cc08e8
Amy Keibler <amelia.keibler@gmail.com>
2021-08-09 01:30:33 +0000

    Implement Applicability
    Actionability
    -------------
    Add the ability to use a regular expression to determine if the project is
    applicable if any file in the project matches the regular expression.
    Add the ability to combine multiple applicability conditions.
    
    Project
    -------
    Add a MonadProject abstraction that manages listing files in the project.
    Create a ProjectContext that stores the project folder we get from Lift and
    implement MonadProject on a ReaderT instance with a ProjectContext.
    Add an allowed-orphans implementation of MonadProject via HMock in the test package.

---
+++ src/Lift/ToolIntegration/Project.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE FlexibleInstances #-}
+module Lift.ToolIntegration.Project
+  ( MonadProject(..)
+  , ProjectContext(..)
+  ) where
+
+import System.Directory
+import System.FilePath.Posix (makeRelative)
+import Relude
+
+class (Monad m) => MonadProject m where
+  listFiles :: m [Text]
+
+instance MonadProject IO where
+  listFiles = error "TODO: implement"
+
+data ProjectContext = ProjectContext
+  { projectRoot :: Text
+  }
+
+instance (MonadIO m) => MonadProject (ReaderT ProjectContext m) where
+  listFiles = do
+    folder <- asks projectRoot
+    let f = toString folder
+    allFiles <- listAllFiles f
+    pure $ toText . makeRelative f <$> allFiles
+
+listAllFiles :: (MonadIO m) => String -> m [String]
+listAllFiles folder = do
+  liftIO $ putStrLn ("listing: " <> folder)
+  contents <- liftIO $ listDirectory (toString folder)
+  (folders, files) <- liftIO $ partitionFiles (mappend (folder <> "/") <$> contents)
+  subFiles <- join <$> traverse listAllFiles folders
+  pure $ files <> subFiles
+  where
+    partitionFiles :: [String] -> IO ([String], [String])
+    partitionFiles = foldlM partitionFile ([], [])
+    partitionFile :: ([String], [String]) -> String -> IO ([String], [String])
+    partitionFile (folders, files) file = do
+      isFile <- doesFileExist file
+      isFolder <- doesDirectoryExist file
+      pure $ case (isFolder, isFile) of
+        (True, False) -> (file : folders, files)
+        (False, True) -> (folders , file : files)
+        (_, _) -> (folders, files)
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,7 @@ import Relude
 
 import qualified Lift.ToolIntegrationSpec as TISpec
 import qualified Lift.ToolIntegration.ApplicableSpec as AS
+import qualified Lift.ToolIntegration.ProjectSpec as PS
 
 main :: IO ()
 main = hspec spec
@@ -13,3 +14,4 @@ spec :: Spec
 spec = do
   describe "Lift.ToolIntegration" TISpec.spec
   describe "Lift.ToolIntegration.Applicable" AS.spec
+  describe "Lift.ToolIntegration.Project" PS.spec
